78092cd4622a7efce58a690f8f4c2358efa00cd4
孙登志 <sundz@paraview.cn>
2021-01-12 15:17:13 +0800

    T# 异步定时任务执行

--- src/main/java/com/sun/springbootdemo/schedul/TimerTask.java
+++ src/main/java/com/sun/springbootdemo/schedul/TimerTask.java
@@ -20,14 +20,14 @@ import java.util.concurrent.TimeUnit;
 public class TimerTask {
 
 
-    @Scheduled(cron = "* 0/1 *  * * ? ")
+    @Scheduled(cron = "* 0/30 *  * * ? ")
     public void excute() throws InterruptedException {
         log.info("定时任务<do1>开始执行了 ==> thread{}:", Thread.currentThread().getName());
         TimeUnit.SECONDS.sleep(6);
 
     }
 
-    @Scheduled(cron = "* 0/1 *  * * ? ")
+    @Scheduled(cron = "* 0/30 *  * * ? ")
     public void excute2() {
         log.info("定时任务<do2>开始执行了 ==> thread{}:", Thread.currentThread().getName());
     }
